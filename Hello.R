ba1c6fec990120724b438fd9d158873ede53d560
Hannah <hng225@lehigh.edu>
2021-09-23 20:22:40 -0400

    Assignment 2A

--- Hello.R
+++ Hello.R
@@ -1,1 +1,8 @@
 cat("Hello, World")
+
+#difference in dates
+Agedays= as.double(as.Date("2021-12-3")-Sys.Date())
+AgeYears= Agedays/365
+
+#run the cat script
+cat ("Hannah, there are", AgeDays, "days left in the semester")
