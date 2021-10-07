6937037f03ac34fba01fc5b19eba76ebd31380ac
Hannah <hng225@lehigh.edu>
2021-10-07 18:08:44 -0400

    Assignment 03B

---
+++ Data/Assignment 03B.R
@@ -0,0 +1,34 @@
+# assignment 03B
+
+
+library("here")
+library("tidyverse")
+PRESIDENT <- read_csv(here("Data", "PRESIDENT_precinct_general.zip"))
+View(PRESIDENT)
+
+results <- read_csv(here("Data","PRESIDENT_precinct_general.zip"))
+
+BIDEN <- 0
+TRUMP <- 0
+COUNTER <- 0
+END <- length(results$votes)
+
+for(i in 1:END) {
+  if(results$candiate[i]=="JOSEPH R BIDEN") {
+    BIDEN <- results$votes [i]+BIDEN
+  }
+}
+
+else if (results$candidate[i]=="DONALD J TRUMP") {
+  Trump <- results$votes [i]+Trump
+}
+
+else{}
+if(round(i/END*100, digits=0)>COUNTER){
+  COUNTER <- round(i/END*100, digits=0)
+  cat(COUNTER, "pct complete\n")
+}
+
+cat("Number of votes for Biden",BIDEN,"\n")
+cat("Number of votes for Trump",Trump)
+
